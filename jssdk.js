334183c39ce3ee101e0e78db2364062e56da59b1
sun-zw <619650434@qq.com>
2018-02-01 13:43:51 +0800

    Update jssdk.js

--- jssdk.js
+++ jssdk.js
@@ -3,20 +3,15 @@ const Jsapi = require('./Jsapi');
 const app = new Koa();
 
 const config = {
-    appId: 'wx0f0810fd20a057b1',
-    appsecret: '03e51c5eea9ce608757b0887f99e8ead',
+    appId: '',
+    appsecret: '',
 }
 app.use(async(ctx, next) => {
     console.time('calculate time:');
     if (ctx.request.path == '/favicon.ico') { return; }
 
     let sdk = new Jsapi(config.appId, config.appsecret);
-    // let accessTokenPromise = sdk.access_token();
-    // let jsapiTicketPromise = sdk.jsapi_ticket();
-    // let signPromise = sdk.sign('url');//url 需动态获取；
-    // let access_token = await accessTokenPromise;
-    // let jsapi_ticket = await jsapiTicketPromise;
-    // let sign = await signPromise;
+
     let access_token = await sdk.access_token();
     let jsapi_ticket = await sdk.jsapi_ticket();
     let sign = await sdk.sign('url');//url 需动态获取；
